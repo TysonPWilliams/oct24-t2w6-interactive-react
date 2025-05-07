6215fb42d414fecfee81caea0c80e5e5f0c2c6da
TysonPWilliams <tyson.williams95@gmail.com>
2025-05-07 20:29:08 +1000

    feat: button to get pokemon data

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,29 @@ class App extends React.Component {
     super();
     this.state = {
       pokemonId: 0,
-      pokemonName: ''
+      pokemonName: '',
+      pokemonSpriteUrl: ""
     }
+
+    this.getRandomPokemon = this.getRandomPokemon.bind(this)
   }
 
   async componentDidMount() {
     console.log('Component mounted');
+
+    this.getRandomPokemon();
+  }
+
+  componentDidUpdate() {
+    console.log(this.state);
+  }
+
+  componentWillUnmount() {
+    console.log('API stuff is all done, goodbye!');
+  }
+
+
+  async getRandomPokemon() {
 
     let randomPokemonId = Math.floor(Math.random() * 1025) + 1;
     console.log("Random Pokemon ID to get is: " + randomPokemonId);
@@ -28,17 +45,11 @@ class App extends React.Component {
         // pokemonId: previousState.pokemonId,
         // And then overwrite the specific bits that we want to update
         pokemonName: data.name,
-        pokemonId: data.id
+        pokemonId: data.id,
+        pokemonSpriteUrl: data.sprites.other.home.front_default
       }
     })
-  }
-
-  componentDidUpdate() {
-    console.log(this.state);
-  }
-
-  componentWillUnmount() {
-    console.log('API stuff is all done, goodbye!');
+    
   }
 
   render() {
@@ -46,11 +57,21 @@ class App extends React.Component {
       <>
         <h1>This is a class component</h1>
 
+        <button onClick={this.getRandomPokemon}>
+          Get a random Pokemon
+        </button>
+
         {this.state.pokemonName.length > 0 && 
         <h1>
           {this.state.pokemonName}
         </h1>
         }
+
+        {this.state.pokemonSpriteUrl.length > 0 &&
+          <img src={this.state.pokemonSpriteUrl} />
+        }
+
+        
 
 
       </>
