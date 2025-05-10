29da6a09d7802035c5bee8c0241aaf054b6922e0
TysonPWilliams <tyson.williams95@gmail.com>
2025-05-10 10:15:25 +1000

    Added PokemonSearcher.jsx and changed from classes to function hooks

--- src/components/PokemonSearcher.jsx.jsx
+++ src/components/PokemonSearcher.jsx.jsx
@@ -1,5 +1,92 @@
-import React from 'react'
-import './App.css'
+import React, { useState } from 'react'
+import '../App.css'
+
+
+export function PokemonSearcher() {
+
+  let [pokemonData, setPokemonData] = useState({});
+  let [pokemonName, setPokemonName] = useState('');
+  let [pokemonSpriteUrl, setPokemonSpriteUrl] = useState('');
+  let [pokemonId, setPokemonId] = useState(0);
+  let [pokemonSearchTerm, setPokemonSearchTerm] = useState('');
+
+
+  const getRandomPokemon = async () => {
+
+    let randomPokemonId = Math.floor(Math.random() * 1025) + 1;
+    console.log("Random Pokemon ID to get is: " + randomPokemonId);
+
+    getSpecificPokemon(randomPokemonId);
+    
+  }
+
+  const getSpecificPokemon = async (targetPokemonValue) => {
+    let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${targetPokemonValue}`);
+    let data = await response.json();
+
+    console.log(data);
+
+    setPokemonName(data.name);
+    setPokemonSpriteUrl(data.sprites.other.home.front_default);
+    setPokemonId(data.id);
+
+    // setPokemonData((previousState) => {
+    //   return {
+    //     // Guarentee that the previous state is not lost
+    //     ...previousState,
+    //     // Overwrite the specific bits that we want to keep
+    //     pokemonName: data.name,
+    //     pokemonId: data.id,
+    //     pokemonSpriteUrl: data.sprites.other.home.front_default
+    //   }
+    // });
+  } 
+
+  return (
+    <>
+        <h1>This is a function component</h1>
+
+        <button onClick={getRandomPokemon}>
+          Get a random Pokemon
+        </button>
+
+        <section>
+          <label htmlFor="pokemonNameInput">Pokemon to search for: </label>
+          <input 
+            type="search" 
+            name="pokemonNameInput" 
+            id="pokemonNameInput" 
+            value={pokemonSearchTerm}
+            onChange={(event) => {
+              setPokemonSearchTerm(event.target.value);
+            }}
+            onKeyDown={(event) => {
+              if (event.key === 'Enter') {
+                getSpecificPokemon(pokemonSearchTerm);
+              }
+            }}
+          />
+          <button onClick={() => getSpecificPokemon(pokemonSearchTerm)}>
+            Search!
+          </button>
+        </section>
+
+        {pokemonName.length > 0 && 
+        <h1>
+          {pokemonName}
+        </h1>
+        }
+
+        {pokemonSpriteUrl.length > 0 &&
+          <img src={pokemonSpriteUrl} />
+        }
+
+      </>
+    )
+  }
+
+
+
 
 class App extends React.Component {
   constructor() {
